120b3c71d7cfd0d48d2a34be9f939f288a1cfd4c
Nahid <nahidshaiket10300@gmail.com>
2016-10-21 21:46:21 +0600

    Routing continued 3

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
@@ -11,12 +12,16 @@ import { HeroService } from './hero.service';
 })
 export class DashboardComponent implements OnInit{ 
 	heroes: Hero[] = [];
-	constructor(private heroService: HeroService){ }
+	constructor(private heroService: HeroService,
+		private router: Router){ }
 
 	ngOnInit(): void{
 		this.heroService.getHeroes()
 			.then(heroes => this.heroes = heroes.slice(0, 5));
 	}
 
-	gotoDetail(hero: Hero): void{ }
+	gotoDetail(hero: Hero): void{
+		let link = ['/detail', hero.id];
+		this.router.navigate(link);
+	}
 }
